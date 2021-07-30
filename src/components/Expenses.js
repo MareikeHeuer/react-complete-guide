8587c41186c4d262664b3b7074812824174d0e62
Mareike Heuer <mareike.heuer@hotmail.com>
2021-07-30 13:23:54 +0200

    Coding challenge: Create separate Expenses component

---
+++ src/components/Expenses.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses({ expenses }) {
+  return (
+    <div className="expenses">
+      {expenses.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
