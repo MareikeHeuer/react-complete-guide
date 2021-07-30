8a3cb52f8788dff0f38a02ebd99bf97fe87c68f5
Mareike Heuer <mareike.heuer@hotmail.com>
2021-07-30 10:15:39 +0200

    Building first custom component: ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>June 20 2021</div>
+      <div>
+        <h2>Birthday Present</h2>
+      </div>
+      <div>$214.15</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
