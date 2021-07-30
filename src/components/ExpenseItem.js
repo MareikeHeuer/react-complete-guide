2ba57b8256b7b1fe66e39fbc545255be80cc472f
Mareike Heuer <mareike.heuer@hotmail.com>
2021-07-30 10:24:07 +0200

    Add styling to ExpenseItem Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>June 20 2021</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Birthday Present</h2>
       </div>
-      <div>$214.15</div>
+      <div className="expense-item__price">$214.15</div>
     </div>
   );
 }
